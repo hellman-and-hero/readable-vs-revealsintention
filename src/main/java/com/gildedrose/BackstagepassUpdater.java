2f3d54af350fb0b5e0a39aa63045b6dd93451ca6
Peter Fichtner (pfichtner) <github.07.fichtner@spamgourmet.com>
2019-05-07 00:18:43 +0200

    initial revision

---
+++ src/main/java/com/gildedrose/BackstagepassUpdater.java
@@ -0,0 +1,29 @@
+package com.gildedrose;
+
+public class BackstagepassUpdater {
+
+	public void update(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+
+			if (item.sellIn < 11) {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1;
+				}
+			}
+
+			if (item.sellIn < 6) {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1;
+				}
+			}
+		}
+
+		item.sellIn = item.sellIn - 1;
+
+		if (item.sellIn < 0) {
+			item.quality = item.quality - item.quality;
+		}
+	}
+
+}
